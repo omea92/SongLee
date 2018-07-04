77355a486bf56f8ea7ea80715582cd486d1ac622
AhnYeSol <dpthf403@naver.com>
2018-07-04 09:43:17 +0900

    정원준 1. 로그인, 회원가입, 메인

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var express = require('express'),
     http = require('http');
 var app = express();
 
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+//app.engine('ejs', require('ejs').renderFile);
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 
@@ -14,6 +14,12 @@ app.get('/', function(req, res){
 
 app.get('/mypage', function(req, res){
   res.render('mypage');
+})
+app.get('/login', function(req, res){
+  res.render('login');
+})
+app.get('/join', function(req, res){
+  res.render('join');
 })
 
 app.listen(3000, function(){
